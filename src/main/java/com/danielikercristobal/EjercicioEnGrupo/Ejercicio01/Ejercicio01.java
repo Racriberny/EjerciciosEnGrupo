348f03a8fc8962048cf525ebaa033b6a2515a1eb
Cristobal <cristobaljunior9@gmail.com>
2021-11-29 18:52:35 +0100

    Prueba

---
+++ src/main/java/com/danielikercristobal/EjercicioEnGrupo/Ejercicio01/Ejercicio01.java
@@ -0,0 +1,7 @@
+package com.danielikercristobal.EjercicioEnGrupo.Ejercicio01;
+
+public class Ejercicio01 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/danielikercristobal/EjercicioEnGrupo/Main.java
@@ -0,0 +1,4 @@
+package com.danielikercristobal.EjercicioEnGrupo;
+
+public class Main {
+}
